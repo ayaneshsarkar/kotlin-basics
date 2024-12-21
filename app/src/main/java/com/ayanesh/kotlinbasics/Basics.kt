3945180f3923ba0c0e9090637c0e15b18c931268
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2024-12-21 22:57:23 +0530

    completed variable quiz

--- app/src/main/java/com/ayanesh/kotlinbasics/Basics.kt
+++
@@ -1,5 +0,0 @@
-package com.ayanesh.kotlinbasics
-
-fun main() {
-  println("Hello World!")
-}
---
+++ app/src/main/java/com/ayanesh/kotlinbasics/VariableType.kt
@@ -0,0 +1,10 @@
+package com.ayanesh.kotlinbasics
+
+fun main() {
+  val constantNumber = 1
+  var mutableString = "Learning Kotlin"
+  mutableString = "Learning Kotlin and feeling great!"
+
+  println(constantNumber)
+  println(mutableString)
+}
