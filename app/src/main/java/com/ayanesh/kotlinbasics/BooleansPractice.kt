9454500205dafe39d49e1c152d8e3f6994280c96
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2024-12-22 09:53:29 +0530

    added BooleansPractice.kt

---
+++ app/src/main/java/com/ayanesh/kotlinbasics/BooleansPractice.kt
@@ -0,0 +1,10 @@
+package com.ayanesh.kotlinbasics
+
+fun main() {
+  val myTrue = true
+  val myFalse = false
+
+  println(myTrue || myFalse) // This will be true
+  println(myTrue && myFalse) // This will be false
+  println(!myTrue) // This will be false
+}
